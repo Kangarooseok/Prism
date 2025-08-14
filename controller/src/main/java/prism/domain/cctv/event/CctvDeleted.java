b889599d81f6770a18f06a94fc9ded9ee1ba3e68
강민석 <kms860926@gmail.com>
2025-08-14 14:36:24 +0900

    modify : kafka 관련 코드 제거

--- controller/src/main/java/prism/domain/cctv/event/CctvDeleted.java
+++ controller/src/main/java/prism/domain/cctv/event/CctvDeleted.java
@@ -1,27 +1,27 @@
-package prism.domain.cctv.event;
-
-import java.util.*;
-import lombok.*;
-import prism.domain.cctv.model.Cctv;
-import prism.infra.AbstractEvent;
-
-// CCTV 삭제 이벤트 객체
-@Setter
-@Getter
-@ToString
-public class CctvDeleted extends AbstractEvent {
-
-    private Long id;
-    private String locationName;
-    private String locationAddress;
-    private String ipAddress;
-    private String hlsAddress;
-    private Date createdAt;
-    private Date updatedAt;
-    private String status;
-
-    // 삭제된 CCTV 정보로 이벤트 초기화
-    public CctvDeleted(Cctv cctv) {
-        super(cctv, "prism.cctv");
-    }
-}
+//package prism.domain.cctv.event;
+//
+//import java.util.*;
+//import lombok.*;
+//import prism.domain.cctv.model.Cctv;
+//import prism.infra.AbstractEvent;
+//
+//// CCTV 삭제 이벤트 객체
+//@Setter
+//@Getter
+//@ToString
+//public class CctvDeleted extends AbstractEvent {
+//
+//    private Long id;
+//    private String locationName;
+//    private String locationAddress;
+//    private String ipAddress;
+//    private String hlsAddress;
+//    private Date createdAt;
+//    private Date updatedAt;
+//    private String status;
+//
+//    // 삭제된 CCTV 정보로 이벤트 초기화
+//    public CctvDeleted(Cctv cctv) {
+//        super(cctv, "prism.cctv");
+//    }
+//}
--- controller/src/main/java/prism/domain/cctv/event/CctvModified.java
+++ controller/src/main/java/prism/domain/cctv/event/CctvModified.java
@@ -1,29 +1,29 @@
-package prism.domain.cctv.event;
-
-import java.util.*;
-import lombok.*;
-import prism.domain.cctv.model.Cctv;
-import prism.infra.AbstractEvent;
-
-// CCTV 수정 이벤트 객체
-@Setter
-@Getter
-@ToString
-public class CctvModified extends AbstractEvent {
-
-    private Long id;
-    private String locationName;
-    private String locationAddress;
-    private String ipAddress;
-    private String hlsAddress;
-    private Float longitude;
-    private Float latitude;
-    private Date createdAt;
-    private Date updatedAt;
-    private String status;
-
-    // 수정된 CCTV 정보로 이벤트 초기화
-    public CctvModified(Cctv cctv) {
-        super(cctv, "prism.cctv");
-    }
-}
+//package prism.domain.cctv.event;
+//
+//import java.util.*;
+//import lombok.*;
+//import prism.domain.cctv.model.Cctv;
+//import prism.infra.AbstractEvent;
+//
+//// CCTV 수정 이벤트 객체
+//@Setter
+//@Getter
+//@ToString
+//public class CctvModified extends AbstractEvent {
+//
+//    private Long id;
+//    private String locationName;
+//    private String locationAddress;
+//    private String ipAddress;
+//    private String hlsAddress;
+//    private Float longitude;
+//    private Float latitude;
+//    private Date createdAt;
+//    private Date updatedAt;
+//    private String status;
+//
+//    // 수정된 CCTV 정보로 이벤트 초기화
+//    public CctvModified(Cctv cctv) {
+//        super(cctv, "prism.cctv");
+//    }
+//}
